d225572b36518b698a7dda01909bf54565845f6c
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-04-12 15:30:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+var student = {
+  name: "Ankit",
+};
+
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+console.log(student.name);
+
+const keys = student.getKeys();
+console.log(keys);
